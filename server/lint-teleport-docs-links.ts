ec23b9068d53ec16121db6de3b3da32acf420f0b
Paul Gottschling <paul.gottschling@gmail.com>
2025-01-31 15:08:32 -0500

    Edit the absolute link linter (#126)
    Allow JavaScript code as the value of the `href` attribute. This allows
    us to import assets as MDX modules so we can refer to the asset paths in
    HTML tags. Otherwise, the linter encounters a runtime exception and
    fails even if there is a comment that ignores messages from the linter.
    
    An example of the linter failing is gravitational/teleport#51193, which
    imports a bot avatar logo as a module so users can download it.

--- server/lint-teleport-docs-links.ts
+++ server/lint-teleport-docs-links.ts
@@ -24,6 +24,11 @@ const isMdxComponentWithHref = (node: Node): node is MdxAnyElement => {
 };
 
 const isAnAbsoluteDocsLink = (href: string): boolean => {
+  // The href is not a string value, and is likely an AST node object. Skip
+  // the absolute link check.
+  if (typeof href != "string") {
+    return false;
+  }
   return (
     href.startsWith("/docs") || href.startsWith("https://goteleport.com/docs")
   );
@@ -35,7 +40,9 @@ export const remarkLintTeleportDocsLinks = lintRule(
     visit(root, undefined, (node: Node) => {
       if (node.type == "link" && isAnAbsoluteDocsLink((node as Link).url)) {
         vfile.message(
-          `Link reference ${(node as Link).url} must be a relative link to an *.mdx page`,
+          `Link reference ${
+            (node as Link).url
+          } must be a relative link to an *.mdx page`,
           node.position
         );
         return;
