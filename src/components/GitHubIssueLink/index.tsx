331861e56d5914efab5e96659172d6712cbdd4fb
Paul Gottschling <paul.gottschling@gmail.com>
2025-05-16 09:24:11 -0400

    Add a custom 404 component (#216)
    Closes #121
    Closes #64
    
    Replace the default Docusaurus 404 component with one that is more
    helpful to docs users.
    
    Add the following links to the 404 page so a user can find content
    similar to what they are looking for:
    
    - A link to the nearest category index page, reducing the likelihood
      that a user needs to navigate through the docs sidebar tree manually.
      For example, a visit to `/docs/enroll-resources/blah` would result in
      a link to `/docs/enroll-resources/`.
    - A link to any pages with the same slug as the 404ing page, in case we
      moved the page but did not provide a redirect.
    - A link to the main docs home page.
    
    This change also adds a search bar so the user can use what is often the
    most helpful tool for navigating to Teleport docs content.
    
    Finally, this change adds a link to create a GitHub issue with the
    404ing path so we can rectify any problems on our end.

--- src/components/GitHubIssueLink/index.tsx
+++ src/components/GitHubIssueLink/index.tsx
@@ -1,36 +1,10 @@
 import styles from "./GitHubIssueLink.module.css";
+import { getReportIssueURL } from "/src/utils/github-issue";
 
 export interface GitHubIssueLinkProps {
   pathname: string;
 }
 
-const getIssueTitle = function (pagePath: string) {
-  return encodeURIComponent(`[docs] ${pagePath}: <DESCRIBE YOUR ISSUE>`);
-};
-
-const getIssueBody = function (pagePath: string) {
-  return encodeURIComponent(`## Applies To
-
-${pagePath}
-
-## Details
-
-<!-- Describe the documentation improvements you wish to see. -->
-
-## How will we know this is resolved?
-
-<!-- Briefly describe a test we can carry out. Scope this as narrowly as possible. -->
-
-## Related Issues
-
-<!-- Please make an effort to find related issues on GitHub and list them here. This makes a big difference in how we prioritize and schedule work. -->
-`);
-};
-
-export const getReportIssueURL = function (pagePath: string): string {
-  const mdxPath = "`" + pagePath + "`";
-  return `https://github.com/gravitational/teleport/issues/new?template=documentation.md&labels=documentation&title=${getIssueTitle(mdxPath)}&body=${getIssueBody(mdxPath)}`;
-};
 export const GitHubIssueLink = function ({ pathname }: GitHubIssueLinkProps) {
   return (
     <p className={styles.githubLink}>
