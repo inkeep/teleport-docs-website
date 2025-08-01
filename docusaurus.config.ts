e64ff013835cc9e0ecad9f29163c0f5e0cfdcb7d
Taras <9948629+taraspos@users.noreply.github.com>
2025-08-01 15:06:02 +0100

    add noIndex: true for preview envs (#287)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,6 +127,7 @@ const config: Config = {
   // contain a trailing slash in the URL, so add trailing slashes to sitemap
   // URLs to prevent clients from receiving non-200 responses.
   trailingSlash: true,
+  noIndex: process.env.AWS_BRANCH_NAME !== "main",
 
   markdown: {
     parseFrontMatter: async (params) => {
