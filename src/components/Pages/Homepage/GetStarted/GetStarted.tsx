2ab2edd57b94307ec3233b039f9fef1344a232cb
Aatu Väisänen <70265716+aatuvai@users.noreply.github.com>
2025-08-22 09:03:37 +0300

    Docs Homepage: Get Started section (#291)
    * Temporarily point submodule to test branch
    
    * Point submodule temporarily to test branch
    
    * Add the GetStarted component
    
    * Fix pointing submodules to base
    
    * Temporarily point submodule to forked repo while testing
    
    * Better typing, match on-hover color change for link
    
    * Add variables for section max-width and bottom padding
    
    * Trigger checks
    
    * Revert submodule test branch

---
+++ src/components/Pages/Homepage/GetStarted/GetStarted.tsx
@@ -0,0 +1,72 @@
+import styles from "./GetStarted.module.css";
+
+interface GetStartedStep {
+  title: string;
+  description: string;
+  href: string;
+}
+
+interface GetStartedLink {
+  title: string;
+  description: string;
+  href: string;
+  iconComponent: any;
+}
+
+interface GetStartedProps {
+  title: string;
+  youtubeVideoUrl: string;
+  steps?: GetStartedStep[];
+  links?: GetStartedLink[];
+}
+
+const GetStarted: React.FC<GetStartedProps> = ({
+  title = "Get Started",
+  youtubeVideoUrl,
+  steps = [],
+  links = [],
+}) => {
+  const getEmbedYouTubeUrl = (url: string) => {
+    const videoId = url.split("v=")[1];
+    return `https://www.youtube.com/embed/${videoId}`;
+  };
+  return (
+    <section className={styles.getStarted}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.grid}>
+          <div className={styles.steps}>
+            {steps.map((step, i) => (
+              <a key={i} href={step.href} className={styles.step}>
+                <h3 className={styles.stepTitle}>{step.title}</h3>
+                <p className={styles.stepDescription}>{step.description}</p>
+              </a>
+            ))}
+          </div>
+          <div className={styles.video}>
+            <iframe
+              src={getEmbedYouTubeUrl(youtubeVideoUrl)}
+              title={title}
+              allowFullScreen
+              className={styles.videoIframe}
+              style={{ width: "100%", height: "100%", border: "none" }}
+            ></iframe>
+          </div>
+          <div className={styles.links}>
+            {links.map((link, i) => (
+              <a href={link.href} key={i} className={styles.link}>
+                <div className={styles.linkContent}>
+                  <h3 className={styles.linkTitle}>{link.title}</h3>
+                  <p className={styles.linkDescription}>{link.description}</p>
+                  <link.iconComponent className={styles.linkIcon} />
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
