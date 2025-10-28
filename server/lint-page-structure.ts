7bc15ae487f08bcc544a8071180a9cb2c4f6b537
Paul Gottschling <paul.gottschling@gmail.com>
2025-10-28 15:50:48 -0400

    Bump expected Node version and add merge check (#437)
    Address issues with the docs engine source that cause lint jobs to
    unexpectedly fail:
    
    - Update the required Node version to 22 in package.json. This is in
      line with the Node version in the workflow that checks component and
      linter changes on docs pull requests. Currently, the default version
      is 18, which causes `server/lint-page-structure` to fail.
    - Prevent a pull request against a linter from failing if we merge an
      unanticipated input in a `gravitational/teleport` submodule between
      approving and merging the linter PR.
    
    Revert "Add a remark linter for improper `Var` use (#387)"
    
    This reverts commit 5f359680c374156cd5a0bb0f7e2cc5a92f4198cd.

--- server/lint-page-structure.ts
+++ server/lint-page-structure.ts
@@ -1,12 +1,7 @@
 import { lintRule } from "unified-lint-rule";
 import { visit } from "unist-util-visit";
-import type { Heading, Code, Paragraph, Text } from "mdast";
-import type {
-  MdxJsxFlowElement,
-  EsmNode,
-  MdxAnyElement,
-  MdxastNode,
-} from "./types-unist";
+import type { Heading, Paragraph, Text } from "mdast";
+import type { MdxJsxFlowElement } from "./types-unist";
 import type { Node, Parent, Position } from "unist";
 
 const mdxNodeTypes = new Set(["mdxJsxFlowElement", "mdxJsxTextElement"]);
@@ -91,7 +86,7 @@ export const remarkLintPageStructure = lintRule(
       vfile.message(
         "This guide is missing at least one introductory paragraph before the first H2. Use introductory paragraphs to explain the purpose and scope of this guide. " +
           messageSuffix,
-        h2s[0].node.position,
+        h2s[0].node.position
       );
     }
 
@@ -100,10 +95,9 @@ export const remarkLintPageStructure = lintRule(
       vfile.message(
         "In a how-to guide, the first H2-level section must be called `## How it works`. Use this section to include 1-3 paragraphs that describe the high-level architecture of the setup shown in the guide. " +
           messageSuffix,
-        h2s[0].node.position,
+        h2s[0].node.position
       );
     }
-
     const stepNumbers: Array<stepNumber> = [];
     h2s.forEach((heading) => {
       const parts = heading.node.value.match(stepNumberPattern);
@@ -126,76 +120,9 @@ export const remarkLintPageStructure = lintRule(
         vfile.message(
           `This guide has an incorrect sequence of steps - expecting a section called "## Step ${expectedNumerator}/${expectedDenominator}". ` +
             messageSuffix,
-          stepNumbers[i].position,
+          stepNumbers[i].position
         );
       }
     }
-
-    const varPattern = /<Var[^>]+\/?>/g;
-    const varNameQuotePattern = /name\s*=\s*(["'])/;
-    const varNames = new Map();
-    // The first position of each Var. We only need one since we only use this
-    // for single-instance Vars.
-    const varPositions = new Map();
-
-    visit(root, undefined, (node: Node) => {
-      // Collect the names of Vars that are outside of code blocks.
-      const el = node as MdxAnyElement;
-      if (
-        (node.type == "mdxJsxTextElement" ||
-          node.type == "mdxJsxFlowElement") &&
-        el.name == "Var"
-      ) {
-        el.attributes.forEach((a) => {
-          if (a.name == "name") {
-            if (!varNames.has(a.value)) {
-              varNames.set(a.value, 0);
-              varPositions.set(a.value, el.position);
-              return;
-            }
-            varNames.set(a.value, varNames.get(a.value) + 1);
-          }
-        });
-      }
-
-      // In a code block, Vars are strings, so find them using regular
-      // expressions.
-      const code = node as Code;
-      if (code.type == "code" || code.type == "inlineCode") {
-        const vars = code.value.matchAll(varPattern);
-        vars.forEach((v) => {
-          // Determine if the "name" param uses single or double quotes.
-          const varQuote = v[0].match(varNameQuotePattern);
-          if (!varQuote) {
-            vfile.message(
-              `Var component found without a valid name`,
-              node.position,
-            );
-          }
-
-          // Extract the name. We know that there is a valid name parameter, so
-          // the array indices are guaranteed to be in range.
-          const namePattern = `name\\s*=\\s*${varQuote[1]}([^${varQuote[1]}]+)${varQuote[1]}`;
-          const varName = v[0].match(new RegExp(namePattern))[1];
-          if (!varNames.has(varName)) {
-            varNames.set(varName, 0);
-            varPositions.set(varName, code.position);
-            return;
-          }
-          varNames.set(varName, varNames.get(varName) + 1);
-        });
-      }
-    });
-
-    varNames.forEach((val, key) => {
-      if (val > 0) {
-        return;
-      }
-      vfile.message(
-        `There is only a single instance of the Var named "${key}" on this page. Add another instance, making it explicit that the user can assign the variable. ` +
-          messageSuffix,
-        varPositions.get(key),
-      );
-    });
-  },
+  }
 );
